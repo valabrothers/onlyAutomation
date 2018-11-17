bfee5fa94e23e86d514f1c0bc2b5784ff8f7c650
Alpesh <alpesh.vala@gmail.com>
2018-11-17 15:08:11 +0530

    updated for compilation error

--- src/main/java/browserHelper/realBrowser.java
+++ src/main/java/browserHelper/realBrowser.java
@@ -2,6 +2,11 @@ package browserHelper;
 
 import java.io.IOException;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
 import commonUtils.propertyFileUtil;
 
 public class realBrowser {
@@ -9,9 +14,8 @@ public class realBrowser {
 	public static realBrowser realbrowser;
 	
 	private realBrowser() throws IOException {
-	startBrowser();	
+		startBrowser();	
 	}
-	
 	
 	//Singleton to ensure only one instance exists of realbrowser
 	public static realBrowser getInstance() throws IOException {
@@ -23,13 +27,28 @@ public class realBrowser {
 		return realbrowser;
 		
 	}
-	
-	
-	public void startBrowser() throws IOException {
-	
+
+
+	public WebDriver startBrowser() throws IOException {
+		WebDriver driver=null;
 		browsertype=propertyFileUtil.getInstance().getProperty("browser.type");
 		browserbaseurl=propertyFileUtil.getInstance().getProperty("browser.base.url");
-		
-		
+
+		if (browsertype.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+ "/resources/chromedriver.exe");
+			driver=new FirefoxDriver();
+
+		}else if (browsertype.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+ "/resources/geckodriver.exe");
+			driver=new ChromeDriver();
+
+		}else if (browsertype.equalsIgnoreCase("ie")) {
+			System.setProperty("webdriver.ie.driver",System.getProperty("user.dir")+ "/resources/IEDriverServer.exe");
+			driver=new InternetExplorerDriver();
+
+		}
+		return driver;
+
+
 	}
 }
