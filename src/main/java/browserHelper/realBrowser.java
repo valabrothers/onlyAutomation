0d568f8ad7680bfa18fc8475e8fa4ea503768e28
Alpesh <alpesh.vala@gmail.com>
2018-11-17 16:58:43 +0530

    updated

--- src/main/java/browserHelper/realBrowser.java
+++ src/main/java/browserHelper/realBrowser.java
@@ -7,12 +7,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
+import commonUtils.propertyFileUtil;
+
+//import commonUtils.propertyFileUtil;
 
 public class realBrowser {
 
-	
-	/*
-	 * 	private String browsertype;
+
+ 	private String browsertype;
 	public static realBrowser realbrowser;
 	
 	private realBrowser() throws IOException {
@@ -53,6 +55,6 @@ public class realBrowser {
 
 
 	}
-	*/
+	
 	
 }
