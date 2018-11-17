39f54c6728ef0da5910b7ae0a39ab560de7a3e14
Alpesh <alpesh.vala@gmail.com>
2018-11-17 16:49:44 +0530

    updated

--- src/main/java/browserHelper/realBrowser.java
+++ src/main/java/browserHelper/realBrowser.java
@@ -1,13 +1,5 @@
 package browserHelper;
 
-import java.io.IOException;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-
-import commonUtils.propertyFileUtil;
 
 public class realBrowser {
 
